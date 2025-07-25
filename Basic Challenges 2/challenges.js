99835370a9e912e39a6178f72494600889608c8c
Georgios11 <tsompanidis.georgios@gmail.com>
2025-07-25 23:27:48 +0200

    dice Game Simulation done

--- Basic Challenges 2/challenges.js
+++ Basic Challenges 2/challenges.js
@@ -157,7 +157,7 @@ function findFirstNonRepeatingCharacter(str) {
   return null;
 }
 // console.log(findFirstNonRepeatingCharacter('PAOK'));
-console.log(findFirstNonRepeatingCharacter('aabbccdd'));
+// console.log(findFirstNonRepeatingCharacter('aabbccdd'));
 
 /**
  Explanation
@@ -166,3 +166,40 @@ Iterate through the string and add each character to the object. If the characte
 Iterate through the string again and check the object to see if the current character has a count of 1. If it does, return it because it's the first non-repeating character.
 If we make it through the entire string without returning a character, we return null because there are no non-repeating characters.
  */
+
+/**
+ * Challenge: Dice Game Simulation
+Instructions
+Write a function called diceGameSimulation that simulates this dice game. The function should take one argument:
+
+numSimulations: The number of times to simulate the dice game.
+The game rules are if a 7 or 11 are rolled, the player wins and they get a result of win. If a 2, 3 or 12 are rolled they lose and get a result of lose. Anything else and they get a result of roll again.
+
+The function should return an array of objects, where each object represents a simulation result. Each object should contain the following properties:
+
+dice1: The value of the first dice (a random number between 1 and 6).
+dice2: The value of the second dice (a random number between 1 and 6).
+sum: The sum of the two dice values.
+result: The result of the roll, which can be "win", "lose", or "roll again".
+ */
+function diceGameSimulation(num) {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    let dice1 = Math.floor(Math.random() * 6 + 1);
+    let dice2 = Math.floor(Math.random() * 6 + 1);
+    let sum = dice1 + dice2;
+    let result = '';
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = 'lose';
+    } else {
+      result = 'roll again';
+    }
+    arr.push({ dice1, dice2, sum, result });
+  }
+
+  return arr;
+}
+console.log(diceGameSimulation(4));
