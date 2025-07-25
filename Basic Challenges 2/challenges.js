71270112d552e9892846b074c4b4a5b655086177
Georgios11 <tsompanidis.georgios@gmail.com>
2025-07-25 14:45:10 +0200

    Array intersection done

--- Basic Challenges 2/challenges.js
+++ Basic Challenges 2/challenges.js
@@ -25,4 +25,16 @@ function fizzBuzzArray(length) {
   }
   return array;
 }
-console.log(fizzBuzzArray(15));
+// console.log(fizzBuzzArray(15));
+
+// Write a function called arrayIntersection that takes in two arrays and returns an array containing the intersection of the two input arrays (i.e., the common elements that appear in both arrays).
+function arrayIntersection(arr1, arr2) {
+  const intersection = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !intersection.includes(arr1[i]))
+      intersection.push(arr1[i]);
+  }
+
+  return intersection;
+}
+console.log(arrayIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
