8d040b204375c16a84ef6d7a7f4917895da00973
Georgios11 <tsompanidis.georgios@gmail.com>
2025-07-26 15:04:03 +0200

    calculate total sales with tax done

--- High Order Array Methods/challenges.js
+++ High Order Array Methods/challenges.js
@@ -9,4 +9,23 @@ function sumOfEvenSquares(numbers) {
     return sum;
   }, 0);
 }
-console.log(sumOfEvenSquares([1, 2, 3, 4, 5, 6]));
+// console.log(sumOfEvenSquares([1, 2, 3, 4, 5, 6]));
+/**
+ * Challenge: Calculate Total Sales
+Instructions
+Use the given array of product objects below, each with their name, price, and quantity sold. Additionally, you are given a tax rate as a percentage. Write a function called calculateTotalSalesWithTax that takes in an array of product objects, along with the tax rate, and returns the total sales amount including tax.
+
+*/
+const products = [
+  { name: 'Apple', price: 0.5, quantity: 10 },
+  { name: 'Banana', price: 0.3, quantity: 20 },
+  { name: 'Orange', price: 0.6, quantity: 15 },
+];
+function calculateTotalSalesWithTax(products, tax) {
+  const result = products.reduce((sales, product) => {
+    sales += product.quantity * product.price;
+    return sales;
+  }, 0);
+  return result + (result * tax) / 100;
+}
+console.log(calculateTotalSalesWithTax(products, 8));
