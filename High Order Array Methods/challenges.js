215ea705210dc98910e19b5b9a9af2560790e7d5
Georgios11 <tsompanidis.georgios@gmail.com>
2025-07-28 13:48:38 +0200

    analyzeCarMileage done

--- High Order Array Methods/challenges.js
+++ High Order Array Methods/challenges.js
@@ -126,3 +126,40 @@ function isValidIPv4(ip) {
 }
 // console.log(isValidIPv4('123.23.123.123'));
 // console.log(isValidIPv4('03.23.123.123'));
+
+/**
+ * Challenge: Car Mileage Analysis
+Instructions
+You are given an array of car objects, each containing information about a car's make, model, year, and mileage. Your goal is to perform some analysis on the car mileage data using high order array methods.
+
+Implement a function called analyzeCarMileage which takes in an array of car objects and performs the following tasks:
+
+Calculate the average mileage of all cars.
+Find the car with the highest mileage.
+Find the car with the lowest mileage.
+Calculate the total mileage of all cars combined.
+The function should return an object containing the calculated values as properties.
+ */
+const cars = [
+  { make: 'Toyota', model: 'Camry', year: 2020, mileage: 30800 },
+  { make: 'Honda', model: 'Civic', year: 2019, mileage: 32000 },
+  { make: 'Chevrolet', model: 'Impala', year: 2021, mileage: 17500 },
+  { make: 'Audi', model: 'R8', year: 2020, mileage: 13000 },
+  { make: 'Tesla', model: 'Model 3', year: 2018, mileage: 50000 },
+];
+
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((total, cur) => (total += cur.mileage), 0);
+  const averageMileage = totalMileage / cars.length;
+
+  const lowestMileageCar = cars.reduce(
+    (lowest, car) => (car.mileage < lowest.mileage ? car : lowest),
+    cars[0]
+  );
+  const highestMileageCar = cars.reduce(
+    (highest, car) => (car.mileage > highest.mileage ? car : highest),
+    cars[0]
+  );
+  return { totalMileage, averageMileage, highestMileageCar, lowestMileageCar };
+}
+console.log(analyzeCarMileage(cars));
